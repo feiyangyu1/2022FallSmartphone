f6635674148b359b155edbe33abbf997d88bce99
Feiyang Yu <yylm1111@gmail.com>
2022-09-23 18:03:12 -0700

    Creating a TableView

--- BasicTableController/BasicTableController/ViewController.swift
+++ BasicTableController/BasicTableController/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
 
+    @IBOutlet weak var tblView: UITableView!
+    
+    let contacts = ["Fish", "Chip", "Taco"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       // tblView.delegate = self
+       // tblView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row]
+        
+        return cell
+    }
 
 }
 
