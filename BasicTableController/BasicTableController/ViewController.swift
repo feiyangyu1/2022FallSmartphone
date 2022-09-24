964a52adcec180da49e9e63b6b2983e4985ab029
Feiyang Yu <yylm1111@gmail.com>
2022-09-23 17:15:08 -0700

    Commting initial code

---
+++ BasicTableController/BasicTableController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicTableController
+//
+//  Created by macBook on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
